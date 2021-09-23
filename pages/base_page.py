1047e643acc6ca71d59e31860dd084c165f1c98a
lenyagolikov <lenyagolikov@gmail.com>
2021-09-23 04:56:08 +0300

    Moved from MainPage 2 methods (login) TO BasePage for more abstracting. Added BasePageLocators. Added 2 test-cases in test_product_page (login)

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,6 +6,8 @@ from selenium.common.exceptions import (
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from .locators import BasePageLocators
+
 
 class BasePage:
     def __init__(self, browser, url, timeout=2):
@@ -15,6 +17,13 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
 
     def is_element_present(self, how, what):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,6 @@ from pages.product_page import ProductPage
 
 
 @pytest.mark.parametrize('id', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
-@pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser, id):
     link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{id}'
     page = ProductPage(browser, link)
@@ -45,3 +44,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.press_button_add_to_basket()
     page.should_not_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
