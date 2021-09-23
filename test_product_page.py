9685b4fbadcdf67a51feed8015fa68e849cd9f27
lenyagolikov <lenyagolikov@gmail.com>
2021-09-23 03:07:28 +0300

    Added mark.parametrize for test-case: add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,10 @@
+import pytest
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+@pytest.mark.parametrize('id', [0, 1, 2, 3, 4, 5, 6, pytest.param(7, marks=pytest.mark.xfail), 8, 9])
+def test_guest_can_add_product_to_basket(browser, id):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{id}'
     page = ProductPage(browser, link)
     page.open()
 
@@ -10,7 +12,6 @@ def test_guest_can_add_product_to_basket(browser):
     product_price = page.get_product_price()
 
     page.press_button_add_to_basket()
-    page.solve_quiz_and_get_code()
 
     product_name_in_message = page.get_product_name_in_message()
     product_price_in_message = page.get_product_price_in_message()
